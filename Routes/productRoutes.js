4e82cf024684720e29e1f3c03af31d921dcafd6b
Nemitha <nemithanuwansith20@gmail.com>
2025-08-22 20:41:05 +0530

    initializing the project

---
+++ Routes/productRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {  deleteProduct, getProducts, saveProducts } from "../controllers/productController.js";
+
+const productRouter = express.Router();
+
+productRouter.get("/", getProducts);
+productRouter.post("/" , saveProducts);
+productRouter.delete("/:productId" , deleteProduct)
+
+
+export default productRouter;
